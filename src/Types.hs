5f02f981ccac8c3d2d6be0332e28a3077f54ae6a
Toine Hulshof <toine.hulshof@hotmail.nl>
2021-03-31 22:43:10 +0200

    Tried to fix a bug, but not completely finished

--- src/Types.hs
+++ src/Types.hs
@@ -391,6 +391,13 @@ funTypeToList :: Type -> [Type]
 funTypeToList (TypeFun t1 t2) = t1 : funTypeToList t2
 funTypeToList t = [t]
 
+tiExps :: TypeEnv -> [Exp] -> TI (Subst, [Type])
+tiExps _ [] = return (nullSubst, [])
+tiExps env (e:es) = do
+    (s1, t1) <- tiExp env e
+    (s2, t2) <- tiExps (apply s1 env) es
+    return (s2 `composeSubst` s1, t1 : t2)
+
 tiFunCall :: TypeEnv -> FunCall -> TI (Subst, Type)
 tiFunCall e@(TypeEnv env) f@(FunCall n es) = case M.lookup (Fun, n) env of
     Nothing -> throwError $ "function " ++ n ++ " doesn't exist"
@@ -399,10 +406,10 @@ tiFunCall e@(TypeEnv env) f@(FunCall n es) = case M.lookup (Fun, n) env of
         case funType t of 
             Nothing -> if null es then return (nullSubst, retType t) else throwError $ "Number of arguments of " ++ show f ++ " does not correspond with its type"
             Just funT -> if length es /= length (funTypeToList funT) then throwError $ show n ++ " got " ++ show (length es)  ++ " arguments, but expected " ++ show (length (funTypeToList funT)) ++ " arguments" else do
-                ts <- mapM (tiExp e) es
-                s <- zipWithM mgu (funTypeToList funT) (map snd ts)
-                let s1 = foldr1 composeSubst $ map fst ts ++ s
-                return (s1, retType t)
+                (s1, ts) <- tiExps e es
+                s <- zipWithM mgu (apply s1 $ funTypeToList funT) ts
+                let s2 = foldr1 composeSubst s
+                return (s2 `composeSubst` s1, retType t)
 
 tiOp1 :: Op1 -> (Type, Type)
 tiOp1 Min = (TypeBasic IntType, TypeBasic IntType)
