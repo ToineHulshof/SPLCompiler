0d7829a57fda7d415cee190f1b62cb8d9a9930bd
Toine Hulshof <toine.hulshof@hotmail.nl>
2021-04-23 14:34:18 +0200

    Made every code generation function return a state monad

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -3,6 +3,8 @@ module Codegen where
 import Grammar
 import Data.Char
 import Data.Maybe
+import Control.Monad.State
+import Control.Monad
 
 data Instruction
     = LoadConstant Int
@@ -12,6 +14,7 @@ data Instruction
     | AdjustStack Int
     | LoadRegister Register
     | StoreRegister Register
+    | StoreStack Int
     | LoadLocal Int
     | Link Int
     | Unlink
@@ -55,6 +58,7 @@ instance Show Instruction where
     show (BranchTrue s) = "brt " ++ s
     show (LoadRegister r) = "ldr " ++ show r
     show (StoreRegister r) = "str " ++ show r
+    show (StoreStack i) = "sts " ++ show i
     show (LoadLocal i) = "ldl " ++ show i
     show (AdjustStack i) = "ajs " ++ show i
     show (Link i) = "link " ++ show i
@@ -80,42 +84,69 @@ instance Show Instruction where
     show Negation = "neg"
     show NotI = "not"
     show Halt = "halt"
-    
+
+type CG a = StateT Int IO a
+
+new :: CG Int
+new = do
+    s <- get
+    put (s + 1)
+    return s
+
 genCode :: FilePath -> SPL -> IO ()
 genCode f spl = do
-    writeFile f (unlines $ map show (BranchAlways "main" : genSPL spl ++ [Trap Int, Halt])) --"ldc 60\nldc 9\nadd\ntrap 0\nhalt"
+    (instructions, _) <- runStateT (genSPL spl) 0
+    writeFile f (unlines $ map show (BranchAlways "main" : instructions ++ [Trap Int, Halt])) --"ldc 60\nldc 9\nadd\ntrap 0\nhalt"
     --putStrLn "\x1b[32mCompilation successful\x1b[0m"
 
-genSPL :: SPL -> [Instruction]
-genSPL = concatMap genDecl
+genSPL :: SPL -> CG [Instruction]
+genSPL ds = concat <$> mapM genDecl ds
 
-genDecl :: Decl -> [Instruction]
+genDecl :: Decl -> CG [Instruction]
 genDecl (DeclVarDecl (VarDecl _ _ e)) = genExp e
-genDecl (DeclFunDecl (FunDecl "main" args _ vars stmts)) = Label "main" : genStmts stmts
-genDecl (DeclFunDecl (FunDecl n args _ vars stmts)) = Label n : Link 0 : genStmts stmts ++ [Unlink, Return]
+genDecl (DeclFunDecl (FunDecl "main" args _ vars stmts)) = do
+    i <- genStmts stmts
+    return $ Label "main" : i
+genDecl (DeclFunDecl (FunDecl n args _ vars stmts)) = do 
+    i <- genStmts stmts
+    return $ Label n : Link 0 : i ++ [Unlink, StoreStack (-1), Return]
 
-genStmts :: [Stmt] -> [Instruction]
-genStmts = concatMap genStmt
+genStmts :: [Stmt] -> CG [Instruction]
+genStmts ss = concat <$> mapM genStmt ss
 
-genStmt :: Stmt -> [Instruction]
+
+
+genStmt :: Stmt -> CG [Instruction]
 genStmt (StmtFunCall f) = genFunCall f
-genStmt (StmtIf e ss1 ss2) = genExp e ++ [BranchTrue "Then"] ++ genStmts (fromMaybe [] ss2) ++ [BranchAlways "End", Label "Then"] ++ genStmts ss1 ++ [Label "End"]
-genStmt (StmtReturn Nothing) = []--[Return]
-genStmt (StmtReturn (Just e)) = genExp e ++ [StoreRegister ReturnRegister]--, Return]
+genStmt (StmtIf e ss1 ss2) = do
+    i1 <- genExp e
+    i2 <- genStmts (fromMaybe [] ss2)
+    i3 <- genStmts ss1
+    i <- show <$> new
+    return $ i1 ++ [BranchTrue $ "Then" ++ i] ++ i2 ++ [BranchAlways $ "End" ++ i, Label $ "Then" ++ i] ++ i3 ++ [Label $ "End" ++ i, StoreRegister ReturnRegister]
+genStmt (StmtReturn Nothing) = return []--[Return]
+genStmt (StmtReturn (Just e)) = genExp e -- ++ [StoreRegister ReturnRegister]--, Return]
 
-genFunCall :: FunCall -> [Instruction]
+genFunCall :: FunCall -> CG [Instruction]
 -- genFunCall (FunCall _ n args) = concatMap genExp args ++ [BranchSubroutine n, AdjustStack (-length args), LoadRegister ReturnRegister]
-genFunCall (FunCall _ n args) = concatMap genExp args ++ [BranchSubroutine n, LoadRegister ReturnRegister]
+genFunCall (FunCall _ n args) = do
+    i <- concat <$> mapM genExp args
+    return $ i ++ [BranchSubroutine n, LoadRegister ReturnRegister]
 
-genExp :: Exp -> [Instruction]
-genExp (Exp _ o e1 e2) = genExp e1 ++ genExp e2 ++ [genOp2 o]
-genExp (ExpOp1 o e) = genExp e ++ [genOp1 o]
+genExp :: Exp -> CG [Instruction]
+genExp (Exp _ o e1 e2) = do
+    i1 <- genExp e1
+    i2 <- genExp e2
+    return $ i1 ++ i2 ++ [genOp2 o]
+genExp (ExpOp1 o e) = do
+    i <- genExp e
+    return $ i ++ [genOp1 o]
 genExp (ExpBrackets e) = genExp e
 genExp (ExpFunCall f) = genFunCall f
-genExp (ExpField _ n fs) = [LoadLocal (-2)]
-genExp (ExpInt i) = [LoadConstant $ fromInteger i]
-genExp (ExpBool b) = [LoadConstant $ if b then 1 else 0]
-genExp (ExpChar c) = [LoadConstant $ ord c]
+genExp (ExpField _ n fs) = return [LoadLocal (-2)]
+genExp (ExpInt i) = return [LoadConstant $ fromInteger i]
+genExp (ExpBool b) = return [LoadConstant $ if b then 1 else 0]
+genExp (ExpChar c) = return [LoadConstant $ ord c]
 
 genOp2 :: Op2 -> Instruction
 genOp2 Plus = Add
