866274f578d37e08903ee03ccf5965e16354ad5c
Toine Hulshof <toine.hulshof@hotmail.nl>
2021-05-19 11:11:37 +0200

    Fixed printing errors on multiple lines

--- src/Errors.hs
+++ src/Errors.hs
@@ -41,10 +41,12 @@ nes a = a :| []
 removePath :: FilePath -> FilePath
 removePath f = reverse $ takeWhile (/= '/') (reverse f)
 
-removeSpace :: Char -> Char
-removeSpace '\n' = ' '
-removeSpace '\t' = ' '
-removeSpace c = c
+removeSpace :: String -> String
+removeSpace [] = []
+removeSpace ('\n' : s) = removeSpace (' ' : s)
+removeSpace ('\t' : s) = removeSpace (' ' : s)
+removeSpace (' ' : ' ' : s) = removeSpace (' ' : s)
+removeSpace (x : xs) = x : removeSpace xs
 
 showErrorStrings :: NonEmpty String -> String
 showErrorStrings (s :| ss) = s ++ concatMap ("\n  • " ++) ss
@@ -52,7 +54,7 @@ showErrorStrings (s :| ss) = s ++ concatMap ("\n  • " ++) ss
 instance Show Errors where
   show (Errors file lines errors) = join "\n\n" $ map showError (sort errors) where
     showError (Error k e Nothing) = "\x1b[1m" ++ removePath file ++ ": \x1b[31merror:\x1b[0m\x1b[1m " ++ show k ++ showErrorStrings e
-    showError (Error k e (Just ((li, co), c'))) = let c = map removeSpace c' in
+    showError (Error k e (Just ((li, co), c'))) = let c = removeSpace c' in
       "\x1b[1m" ++ removePath file ++ ":" ++ show li ++ ":" ++ show co ++ ": \x1b[31merror:\x1b[0m\x1b[1m " ++ show k ++ showErrorStrings e ++ "\n" ++
       replicate leftLength ' ' ++ "\x1b[34m|\n" ++ show li ++ " |\x1b[0m " ++
       let (l, r) = splitAt (co - 1) (lines ! li) in (l ++ "\x1b[31m\x1b[1m" ++ c ++ "\x1b[0m" ++ drop (length c) r) ++ "\n" ++
--- src/Types.hs
+++ src/Types.hs
@@ -193,7 +193,7 @@ mgu' e _ Void Void ot1 ot2 = return nullSubst
 mgu' e p t1 t2 ot1 ot2 = typeError p ot1 ot2
 
 typeError :: P -> NonEmpty Type -> NonEmpty Type -> TI Subst
-typeError p@(_, a) (h1 :| t1) (h2 :| t2) = tell [Error TypeError (("\x1b[1m\x1b[33m" ++ map removeSpace a ++ "\x1b[0m\x1b[1m has type " ++ showType True (varsMap h1) h1 ++ "\x1b[1m, but is expected to have type " ++ showType True (varsMap h2) h2 ++ "\x1b[1m") :| zipWith extraError t1 t2 ) (Just p)] >> return nullSubst
+typeError p@(_, a) (h1 :| t1) (h2 :| t2) = tell [Error TypeError (("\x1b[1m\x1b[33m" ++ removeSpace a ++ "\x1b[0m\x1b[1m has type " ++ showType True (varsMap h1) h1 ++ "\x1b[1m, but is expected to have type " ++ showType True (varsMap h2) h2 ++ "\x1b[1m") :| zipWith extraError t1 t2 ) (Just p)] >> return nullSubst
     where 
         extraError :: Type -> Type -> String
         extraError t1 t2 = "\x1b[1m-> " ++ showType True (varsMap t1) t1 ++ "\x1b[1m must match " ++ showType True (varsMap t2) t2 ++ "\x1b[1m"
