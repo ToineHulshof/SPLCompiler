720a17ed7ee98d5783c48ea75c76422f5eee15c4
Toine Hulshof <toine.hulshof@hotmail.nl>
2021-05-26 22:21:04 +0200

    a

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import System.Exit ( exitFailure, exitSuccess )
+import System.Process ( readProcess )
+
 main :: IO ()
-main = putStrLn "Tests not implemented yet"
+main = do
+    output <- readProcess "sh" ["testfile.sh", "try.spl"] []
+    if output == "\ESC[32m\9989 Program is correctly typed\ESC[0m\n\n\ESC[36m0\ESC[0m\n\ESC[36m1\ESC[0m\n\ESC[36m2\ESC[0m\n\ESC[36m3\ESC[0m\n\ESC[36m4\ESC[0m\n\ESC[36m5\ESC[0m\n\ESC[36m6\ESC[0m\n\ESC[36m7\ESC[0m\n\ESC[36m8\ESC[0m\n\nmachine halted\n"
+        then exitSuccess
+        else exitFailure
